925b980aba693cf6204129c270f7ee0f761ffb27
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-02-29 20:46:49 +1000

    Fix cancelling downloads

--- resources/lib/download.py
+++ resources/lib/download.py
@@ -2,8 +2,8 @@ from resources.lib.utils.kodi import log, get_setting
 from xbmcgui import DialogProgress
 from routing import Plugin
 import urllib.request
-
 plugin = Plugin()
+
 
 @plugin.route("/download")
 def download_to_disk(url: str):
@@ -17,10 +17,13 @@ def download_to_disk(url: str):
         log("no download dir please do download dir")
 
 # ! RENAME FUNCTION PLEASE
+
+
 def download_hopefully(url, filename, progressbar: DialogProgress):
     try:
         req = urllib.request.urlopen(url)
-        total_size = int(req.getheader('Content-Length')) if req.getheader('Content-Length') else None
+        total_size = int(req.getheader('Content-Length')
+                         ) if req.getheader('Content-Length') else None
         downloaded = 0
 
         with open(filename, "wb") as file:
@@ -29,17 +32,20 @@ def download_hopefully(url, filename, progressbar: DialogProgress):
                 chunk = req.read(1024)
                 if not chunk:
                     break
+                if progressbar.iscanceled():
+                    break
                 downloaded += len(chunk)
                 file.write(chunk)
                 if total_size:
                     progress = downloaded * 100 / total_size
-                    log(f"Downloading... Progress: {int(progress)}%") 
-                    progressbar.update(int(progress), f"Progress: {progress:.2f}%")
+                    log(f"Downloading... Progress: {int(progress)}%")
+                    progressbar.update(
+                        int(progress), f"Progress: {progress:.2f}%")
                 else:
                     log(f"Downloaded {downloaded} bytes...")
                     progressbar.close()
         log("\nDownload complete.")
+
         # ? Return something?
     except Exception as e:
         log(e)
-            
