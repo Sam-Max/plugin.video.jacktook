df93fd009d3ad12b4b96f13922256545ee24690d
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-02-29 20:32:31 +1000

    Select download dir via settings

--- resources/lib/download.py
+++ resources/lib/download.py
@@ -1,24 +1,20 @@
-from resources.lib.utils.kodi import log
+from resources.lib.utils.kodi import log, get_setting
 from xbmcgui import DialogProgress
 from routing import Plugin
-from xbmcgui import ListItem
-from time import sleep
-import requests
 import urllib.request
-# * ill get rid of the unused imports soon before any PR
 
 plugin = Plugin()
 
 @plugin.route("/download")
 def download_to_disk(url: str):
-    log("PLEASE")
     # TODO implement progress dialog
     progress_dialogue = DialogProgress()
-    
-    # TODO implement logic for grabbing direct URL by result info from debrid api, just support cached RD results to start with
-    log(url)
-    
-    download_hopefully(url, r"C:\\Users\\Sam\\Documents\\filename", progress_dialogue)
+    selected_dir = get_setting("download_dir")
+    if selected_dir:
+        download_hopefully(url, f"{selected_dir}/filename2", progress_dialogue)
+    else:
+        # ! THROW ERROR ABOUT DOWNLOAD DIR
+        log("no download dir please do download dir")
 
 # ! RENAME FUNCTION PLEASE
 def download_hopefully(url, filename, progressbar: DialogProgress):
@@ -37,7 +33,7 @@ def download_hopefully(url, filename, progressbar: DialogProgress):
                 file.write(chunk)
                 if total_size:
                     progress = downloaded * 100 / total_size
-                    log(f"Downloading... Progress: {progress}%") 
+                    log(f"Downloading... Progress: {int(progress)}%") 
                     progressbar.update(int(progress), f"Progress: {progress:.2f}%")
                 else:
                     log(f"Downloaded {downloaded} bytes...")
