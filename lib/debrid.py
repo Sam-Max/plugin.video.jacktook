17daabd1f1f7402424a56b5cbaf07836be5904b0
sammax <dsammax@outlook.com>
2024-04-14 11:22:35 +0200

    minor fix & improvement

--- lib/debrid.py
+++ lib/debrid.py
@@ -22,6 +22,7 @@ from lib.utils.utils import (
 
 dialog_update = {"count": -1, "percent": 50}
 
+
 def check_debrid_cached(query, results, mode, media_type, dialog, rescrape, episode=1):
     if not rescrape:
         if query:
@@ -45,7 +46,7 @@ def check_debrid_cached(query, results, mode, media_type, dialog, rescrape, epis
     else:
         total = len(results)
 
-    get_magnet_and_infohash(results, lock)
+    get_magnet_and_infohash(results, lock, dialog)
 
     with ThreadPoolExecutor(max_workers=total) as executor:
         if rd_enabled:
@@ -162,12 +163,11 @@ def check_pm_cached(client, res, cached_results, uncached_result, total, dialog,
         log(f"Error: {str(e)}")
 
 
-def get_magnet_and_infohash(results, lock):
+def get_magnet_and_infohash(results, lock, dialog):
     with lock:
-        for res in results:
+        for count, res in enumerate(results):
             magnet = ""
             info_hash = ""
-
             if guid := res.get("guid"):
                 if guid.startswith("magnet:?") or len(guid) == 40:
                     magnet = guid
@@ -178,7 +178,9 @@ def get_magnet_and_infohash(results, lock):
                     )
                 else:
                     # For some indexers, the guid is a torrent file url
-                    magnet, info_hash = get_magnet_from_uri(res.get("guid"))
+                    magnet, info_hash = get_magnet_from_uri(
+                        res.get("guid"), dialog, count, results
+                    )
 
             if not (magnet and info_hash):
                 url = res.get("magnetUrl", "") or res.get("downloadUrl", "")
@@ -186,33 +188,35 @@ def get_magnet_and_infohash(results, lock):
                     magnet = url
                     info_hash = get_info_hash_from_magnet(url).lower()
                 else:
-                    magnet, info_hash = get_magnet_from_uri(url)
+                    magnet, info_hash = get_magnet_from_uri(url, dialog, count, results)
 
             res["magnet"] = magnet
             res["infoHash"] = info_hash
 
 
-def get_magnet_from_uri(uri):
+def get_magnet_from_uri(uri, dialog, count, results):
     magnet = ""
     info_hash = ""
-
+    dialog.update(
+        0,
+        "Jacktook [COLOR FFFF6B00]Debrid[/COLOR]",
+        f"Extracting Magnet...{count}/{len(results)}",
+    )
     if is_url(uri):
-        res = requests.get(
-            uri, allow_redirects=False, timeout=20, headers=USER_AGENT_HEADER
-        )
-        if res.is_redirect:
-            uri = res.headers.get("Location")
-            if uri.startswith("magnet:"):
-                magnet = uri
-                info_hash = get_info_hash_from_magnet(uri).lower()
-        elif (
-            res.status_code == 200
-            and res.headers.get("Content-Type") == "application/octet-stream"
-        ):
-            magnet = extract_magnet_from_url(uri)
-        else:
-            log(f"Failed to extract torrent data from: {uri}")
-
+        try:
+            res = requests.get(
+                uri, allow_redirects=False, timeout=10, headers=USER_AGENT_HEADER
+            )
+            if res.status_code == 200:
+                if res.is_redirect:
+                    uri = res.headers.get("Location")
+                    if uri.startswith("magnet:"):
+                        magnet = uri
+                        info_hash = get_info_hash_from_magnet(uri).lower()
+                elif res.headers.get("Content-Type") == "application/octet-stream":
+                    magnet = extract_magnet_from_url(uri)
+        except Exception as e:
+            log(f"Failed to extract torrent data from: {str(e)}")
     return magnet, info_hash
 
 
@@ -226,6 +230,3 @@ def debrid_dialog_update(total, dialog, lock):
             f"Jacktook [COLOR FFFF6B00]Debrid[/COLOR]",
             f"Checking: {dialog_update.get('count')}/{total}",
         )
-
-
-
