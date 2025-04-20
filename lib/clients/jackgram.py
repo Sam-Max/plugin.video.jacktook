84d619d332c8830e35d4b0565d6c8aad82d9afe4
sammax <samagd@outlook.es>
2025-04-20 21:49:28 +0200

    refactor: improve response parsing for file extraction

--- lib/clients/jackgram.py
+++ lib/clients/jackgram.py
@@ -1,4 +1,5 @@
 import json
+from lib.api.jacktook.kodi import kodilog
 from lib.clients.base import BaseClient
 from lib.utils.kodi_utils import translation
 
@@ -9,6 +10,8 @@ class Jackgram(BaseClient):
 
     def search(self, tmdb_id, query, mode, media_type, season, episode):
         try:
+            kodilog(f"Searching {query} on Jackgram")
+
             if mode == "tv" or media_type == "tv":
                 url = f"{self.host}/stream/series/{tmdb_id}:{season}:{episode}.json"
             elif mode == "movies" or media_type == "movies":
@@ -16,13 +19,19 @@ class Jackgram(BaseClient):
             else:
                 url = f"{self.host}/search?query={query}"
 
+            kodilog(f"Jackgram URL: {url}")
+            kodilog(f"Jackgram mode: {mode}")
+
             res = self.session.get(url, timeout=10)
             if res.status_code != 200:
                 return
+            
             if mode in ["tv", "movies"]:
                 return self.parse_response(res)
             else:
-                return self.parse_response_search(res)
+                response = self.parse_response_search(res)
+                kodilog(f"Jackgram search response: {response}")
+                return response
         except Exception as e:
             self.handle_exception(f"{translation(30232)}: {e}")
 
@@ -32,7 +41,7 @@ class Jackgram(BaseClient):
         if res.status_code != 200:
             return
         return res.json()
-    
+
     def get_files(self, page):
         url = f"{self.host}/stream/files?page={page}"
         res = self.session.get(url, timeout=10)
@@ -61,17 +70,20 @@ class Jackgram(BaseClient):
         res = res.json()
         results = []
         for item in res["results"]:
-            for file in item["files"]:
-                date = "" if file["date"] == None else file["date"]
-                results.append(
-                    {
-                        "title": file["title"],
-                        "type": "Direct",
-                        "indexer": file["name"],
-                        "size": file["size"],
-                        "publishDate": date,
-                        "duration": file["duration"],
-                        "downloadUrl": file["url"],
-                    }
-                )
+            if item.get("type") == "file":
+                results.append(self._extract_file_info(item))
+            else:
+                for file in item.get("files", []):
+                    results.append(self._extract_file_info(file))
         return results
+
+    def _extract_file_info(self, file):
+        return {
+            "title": file.get("title", ""),
+            "type": "Direct",
+            "indexer": file.get("name", ""),
+            "size": file.get("size", ""),
+            "publishDate": file.get("date", ""),
+            "duration": file.get("duration", ""),
+            "downloadUrl": file.get("url", ""),
+        }
