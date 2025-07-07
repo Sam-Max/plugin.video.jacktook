f308d8b5afd02a4b0a6de6a65a1819e8d45dc891
cleanhands <cleanhands@users.noreply.github.com>
2025-07-07 07:50:37 -0400

    fix: remove duplicate queries and results

--- lib/utils/general/processors.py
+++ lib/utils/general/processors.py
@@ -132,8 +132,6 @@ class PreProcessBuilder(BaseProcessBuilder):
             rf"{season_fill}x{episode_fill}",  # XXxXX format
             rf"\.S{season_fill}E{episode_fill}",  # .SXXEXX format
             rf"\sS{season_fill}E{episode_fill}\s",  # season and episode surrounded by spaces
-            rf"\sS{season_fill}\s",  # season surrounded by spaces
-            rf"\.S{season_fill}\.",  # season surrounded by dots
             r"Cap\.",  # match "Cap."
         ]
 
